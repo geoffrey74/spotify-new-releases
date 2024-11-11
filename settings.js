38a826ff6eab36be022e316c8f8157f8bc3919ae
Geoffrey H. <gholtscl@gmail.com>
2024-11-11 12:45:46 +0100

    Remove default playlist ID from settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
 export const SPOTIFY_BASE_URL = 'https://api.spotify.com/v1';
-export const PLAYLIST_ID = '2fTaHwIH6Uw4zng3MWNbeq';
+export const PLAYLIST_ID = 'playlist_ID_here';
 export const DAY_COUNT = process.env.npm_config_days || 7;
